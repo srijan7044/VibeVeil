1d045229a8ce5455b6e56cfba85e91ec86c1600f
srijan7044 <offsray7044@gmail.com>
2026-02-16 23:20:48 +0530

    send message func. implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import path from "path";
 import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
-import messagesRoutes from "./routes/message.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 
@@ -13,24 +13,23 @@ const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json({ limit: "50mb" })); //req.body
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); //CORS
+app.use(express.json({ limit: "5mb" })); // req.body
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages", messagesRoutes);
+app.use("/api/messages", messageRoutes);
 
-// // make ready for deployment
-
+// make ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 app.listen(PORT, () => {
-  console.log("Server is running on port:" + PORT);
+  console.log("Server running on port: " + PORT);
   connectDB();
 });
