167165d4dd620b9848d7c0e4adf6667faaccb6fb
srijan7044 <offsray7044@gmail.com>
2026-02-20 01:23:31 +0530

    deployment trying

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -14,8 +14,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
-    secure: ENV.NODE_ENV === "development" ? false : true,
+    sameSite: "none", // CSRF attacks
+    secure: true, //ENV.NODE_ENV === "development" ? false : true,
   });
 
   return token;
